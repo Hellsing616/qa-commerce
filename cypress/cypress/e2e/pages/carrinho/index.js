3cec8bc32df609a042bf63db5be5f37bd1af95d7
Hellsing616 <alesilva6162gmail.com>
2026-02-23 03:59:20 -0300

    Ajustes gerais

---
+++ cypress/cypress/e2e/pages/carrinho/index.js
@@ -0,0 +1,26 @@
+class Carrinho{
+
+    visitarPaginaHome(){
+        cy.visit('http://localhost:3000')
+    }
+
+    clickBtnAdicionarCarrinho(){
+        cy.get(':nth-child(4) > .card > .card-body > .btn').click()
+    }
+
+    clickBtnCarrinho(){
+        cy.get(':nth-child(2) > .nav-link').click() 
+    }
+
+    selecionarQtdProduto(){
+
+    }
+
+   limparCarrinho(){
+    
+   }
+
+
+}
+
+export default new Carrinho()
